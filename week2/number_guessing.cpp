ffb6f082e3e34d9fbf139a56a799932b61a8a0e0
Tuesdae Hunter <tuesdae.a.hunter@student.wwcc.edu>
2026-04-30 19:14:47 -0700

    submit week2

---
+++ week2/number_guessing.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    int secretNumber = rand() % 100 + 1;
+    int guess;
+    int attempts = 0;
+
+    cout << "Number Guessing Game" << endl;
+    cout << "I'm thinking of a number between 1 and 100." << endl << endl;
+
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+
+        attempts++;
+
+        if (guess > secretNumber) {
+            cout << "Too high! Try again." << endl << endl;
+        } else if (guess < secretNumber) {
+            cout << "Too low! Try again." << endl << endl;
+        } else {
+            cout << "Congratulations! You guessed the number " << secretNumber << " correctly!" << endl;
+            cout << "It took you " << attempts << " guesses." << endl;
+        }
+
+    } while (guess != secretNumber);
+
+    return 0;
+}
